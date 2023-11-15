4f3a45885c760573819e48d686ed39a4e158acd3
Adam Veselý <113090817+Advy10@users.noreply.github.com>
2023-11-15 09:30:16 +0100

    Scripty z 15.11

--- scripty/script.js
+++ scripty/script.js
@@ -1,31 +1,41 @@
 console.log(`Mikasa Sukasa`);
 
-const MINIMUM = 15;
+const MINIMUM_AGE = 15;
+const MAXIMUM_AGE = 69;
+
 const jmenoUzivatele = window.prompt('Jak se jmenuješ?', 'Milion Plus Fans');
 window.alert(`Nezapomeň! Yzo Empire na věky!, ${jmenoUzivatele}!`);
+const jeFanousekMilionPlus = window.confirm('Jsi fanoušek Milion Plus? Klikni na OK pro Ano, nebo Storno pro Ne.');
 
-let vekUzivatele = window.prompt('Tak a kolik ti je vlastně let?');
-console.log(vekUzivatele);
+if (jeFanousekMilionPlus) {
+  let vekUzivatele = window.prompt('Tak a kolik ti je vlastně let?');
+  console.log(vekUzivatele);
 
-if (Number(vekUzivatele) === MINIMUM) {
-  window.alert(`Gratuluji, můžeš na web!`);
-}
-else if (Number(vekUzivatele) > MINIMUM) {
-  window.alert(`Můžeš na web, je ti o ${vekUzivatele - MINIMUM} víc než minimum ${MINIMUM} let!`);
-}
-else {
   if (!isNaN(Number(vekUzivatele))) {
-    window.alert(`Abych tě pustil na web, musíš počkat ještě ${MINIMUM - vekUzivatele} let!`);
+    if (vekUzivatele >= MINIMUM_AGE && vekUzivatele <= MAXIMUM_AGE) {
+      window.alert(`Gratuluji, můžeš na web!`);
+      window.alert(`Vítej v naší hře na počest Yzomandiasovi!`);
+    } else {
+      window.alert(`Omlouváme se, ale musí ti být nejméně ${MINIMUM_AGE} a nejvíce ${MAXIMUM_AGE} let. Omlouváme se za potíže.`);
+    }
+  } else {
+    const novaSance = window.confirm('Vypadá to, že jsi nezadal(a) číslo. Chceš to zkusit znovu? Klikni na OK pro Ano, nebo Storno pro Ne.');
+    if (novaSance) {
+      vekUzivatele = window.prompt('Dáme ti ještě jednu šanci. Kolik ti je skutečně let?');
+      if (!isNaN(Number(vekUzivatele)) && vekUzivatele >= MINIMUM_AGE && vekUzivatele <= MAXIMUM_AGE) {
+        window.alert(`Vítej v naší hře na počest Yzomandiasovi!`);
+      } else {
+        window.alert(`Omlouváme se, ale musí ti být nejméně ${MINIMUM_AGE} a nejvíce ${MAXIMUM_AGE} let. Pokud opět nezadáš platné číslo, nebude ti umožněn přístup.`);
+      }
+    } else {
+      window.alert(`Omlouváme se, ale musí ti být nejméně ${MINIMUM_AGE} a nejvíce ${MAXIMUM_AGE} let. Pokud opět nezadáš platné číslo, nebude ti umožněn přístup.`);
+    }
   }
-  else {
-    window.alert(`${vekUzivatele} To přece nedává žádný smysl!`);
-  }
+} else {
+  window.alert('Omlouváme se, ale nemůžeš pokračovat, nejsi fanoušek Milion Plus!');
 }
 
 
-window.alert(`Vítej v naší hře na počest Yzomandiasovi!`);
-const nameOfPlayer = window.prompt(`Jak se jmenuješ, uličníku?`, `Yzomandias`);
-window.alert(`Vítám tě, ${nameOfPlayer}, zajímá tě to, co je neznámé, tajemné, nevysvětlitelné, proto jsi přece tady. A my teď poprvé řekneme celou pravdu o tom, co se stalo.`);
 
 
 
